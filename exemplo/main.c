976649db382f6173b96afd99a76df8c20f844975
Herick Oliveira <hkmarques@gmail.com>
2014-04-19 21:47:38 -0300

    Initial Commit

---
+++ exemplo/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  exemplo
+//
+//  Created by Herick Oliveira on 4/19/14.
+//  Copyright (c) 2014 Herick Oliveira. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
